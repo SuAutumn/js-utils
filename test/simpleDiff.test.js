41c654cc847fcb0d355bc9b48e9c66feff43945f
Thorne <1083621486@qq.com>
2021-02-25 11:47:22 +0800

    feat: refactor the simpleDiff.test.js

--- test/simpleDiff.test.js
+++ test/simpleDiff.test.js
@@ -22,47 +22,28 @@ test('simpleDiff: mock', () => {
   expect(simpleDiff([], [])).toEqual([])
 
   expect(simpleDiff([1], [])).toEqual([{ data: 1, type: 'add' }])
-
-  expect(simpleDiff([3, 2, 1], [1])).toEqual([
-    { data: 3, type: 'add' },
-    { data: 2, type: 'add' },
-  ])
+  expect(simpleDiff([], [1])).toEqual([{ data: 1, type: 'delete' }])
 
   expect(simpleDiff([2, 1], [3, 2, 1])).toEqual([{ data: 3, type: 'delete' }])
-
   expect(simpleDiff([3, 1], [3, 2, 1])).toEqual([{ data: 2, type: 'delete' }])
-
   expect(simpleDiff([3, 2], [3, 2, 1])).toEqual([{ data: 1, type: 'delete' }])
 
+  // 收尾diff
   expect(simpleDiff([4, 3, 2], [3, 2, 1])).toEqual([
     { data: 4, type: 'add' },
     { data: 1, type: 'delete' },
   ])
-
-  expect(simpleDiff([5, 4, 3, 2], [3, 2, 1])).toEqual([
-    { data: 5, type: 'add' },
-    { data: 4, type: 'add' },
-    { data: 1, type: 'delete' },
-  ])
-
-  expect(simpleDiff([3, 2, 1], [5, 4, 3, 2])).toEqual([
-    { data: 5, type: 'delete' },
+  expect(simpleDiff([3, 2, 1], [4, 3, 2])).toEqual([
     { data: 4, type: 'delete' },
     { data: 1, type: 'add' },
   ])
 
-  expect(simpleDiff([3, 2], [5, 4, 3, 2, 1])).toEqual([
+  expect(simpleDiff([10, 8, 7, 6, 4, 3, 2], [9, 8, 7, 6, 5, 3, 1])).toEqual([
+    { data: 10, type: 'add' },
+    { data: 9, type: 'delete' },
+    { data: 4, type: 'add' },
     { data: 5, type: 'delete' },
-    { data: 4, type: 'delete' },
-    { data: 1, type: 'delete' },
-  ])
-
-  expect(simpleDiff([8, 7, 6, 3, 2], [5, 4, 3, 2, 1])).toEqual([
-    { data: 8, type: 'add' },
-    { data: 7, type: 'add' },
-    { data: 6, type: 'add' },
-    { data: 5, type: 'delete' },
-    { data: 4, type: 'delete' },
+    { data: 2, type: 'add' },
     { data: 1, type: 'delete' },
   ])
 })
