430e74ce104c94ef0d0b763509c05f8ee74d4f42
Thorne <1083621486@qq.com>
2021-03-16 15:24:35 +0800

    chore: 同步代码

--- utils/request.ts
+++ utils/request.ts
@@ -1,6 +1,9 @@
 import * as https from 'https'
 import * as http from 'http'
 import genPromise from './genPromise.js'
+import formatDate from './formatDate'
+import * as fs from 'fs'
+import * as os from 'os'
 
 interface RequestOptions extends https.RequestOptions {}
 
@@ -9,6 +12,8 @@ interface Response<T> {
   status?: number
   message?: string
 }
+
+const LOG_FILENAME = './assets/socket-error.txt'
 function request(
   url: string,
   options: https.RequestOptions
@@ -25,6 +30,42 @@ function request(
     }
   })
   req.on('error', fail)
+  req.on('socket', (socket) => {
+    const now = Date.now()
+    const time = formatDate(now, 'HH:mm:ss.S')
+    // socket.setEncoding('utf8')
+    socket.on('lookup', (_err, address, family, host) => {
+      log(
+        LOG_FILENAME,
+        time,
+        (Date.now() - now) / 1000 + 's',
+        'lookup',
+        address,
+        family,
+        host
+      )
+    })
+    const eventNameList = ['connect', 'ready', 'close', 'end', 'timeout']
+    eventNameList.forEach((name) => {
+      socket.on(name, () => {
+        log(
+          LOG_FILENAME,
+          time,
+          (Date.now() - now) / 1000 + 's',
+          name,
+          name === 'close' ? os.EOL : ''
+        )
+      })
+    })
+    socket.on('error', (err) => {
+      log(
+        LOG_FILENAME,
+        time,
+        (Date.now() - now) / 1000 + 's',
+        JSON.stringify(err)
+      )
+    })
+  })
   req.end()
   return p
 }
@@ -54,17 +95,31 @@ function getReqBody(req: http.IncomingMessage): Promise<string> {
   return p
 }
 
-get('https://weibo.com/u/1300419694?is_all=1', {
-  headers: {
-    cookie: [
-      'SUB=_2A25NNyDoDeRhGeNJ61QW-SvNwzqIHXVuRRUgrDV8PUNbmtAfLXf_kW9NSBXBn1Yh9I4ChnnjfQMok6Du54vfVjhR',
-      'SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WF0fmhMbsPYJ77qT22HzsTo5NHD95QfS05cS0.feKncWs4Dqcj1-NHWUPxfdgREeh2feh.t',
-    ].join(';'),
-  },
-})
-  .then((res) => {
-    console.log(res.data.length, ' ', Buffer.from(res.data).length, 'B')
+function log(filename: string, ...args: Array<string | number>) {
+  const s = fs.createWriteStream(filename, { flags: 'a+' })
+  s.write(args.join(' ') + os.EOL, (err) => {
+    err && console.log('write error', err)
   })
-  .catch((e) => {
-    console.log(e)
+  s.end()
+}
+
+function _test() {
+  get('https://weibo.com/u/1300419694?is_all=1', {
+    headers: {
+      cookie: [
+        'SUB=_2A25NNyDoDeRhGeNJ61QW-SvNwzqIHXVuRRUgrDV8PUNbmtAfLXf_kW9NSBXBn1Yh9I4ChnnjfQMok6Du54vfVjhR',
+        'SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WF0fmhMbsPYJ77qT22HzsTo5NHD95QfS05cS0.feKncWs4Dqcj1-NHWUPxfdgREeh2feh.t',
+      ].join(';'),
+    },
+    rejectUnauthorized: false,
   })
+    .then((res) => {
+      console.log(res.data.length, Buffer.from(res.data).length / 1024 + 'kb')
+    })
+    .catch((e: Error) => {
+      console.log(e)
+    })
+  setTimeout(_test, 3 * 1000)
+}
+
+_test()
